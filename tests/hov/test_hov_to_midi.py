60a899c01bdb348895b408c7504389e2e303911d
Amon Benson <amonkbenson@gmail.com>
2026-01-29 13:34:50 +0100

    added more tests

--- tests/hov/test_hov_to_midi.py
+++ tests/hov/test_hov_to_midi.py
@@ -30,6 +30,13 @@ def convert(hov: np.ndarray) -> mido.MidiFile:
         return midi
 
 
+def use_absolute_time(track: mido.MidiTrack):
+    t = 0
+    for message in track:
+        t += message.time
+        message.time = t
+
+
 def test_metadata():
     # hov = np.array(
     #     [
@@ -61,17 +68,57 @@ def test_metadata():
 
 
 def test_single_kick():
-    hov = np.array(
-        [
-            [[1.0, 0, 1.0], [0, 0, 0], [0, 0, 0]],  # Step 0
-            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],  # Step 1
-        ],
-        dtype=np.float32,
-    )
+    hov = np.array([
+        [[1.0, 0, 64 / 127], [0, 0, 0], [0, 0, 0]],  # Step 0
+        [[0, 0, 0], [0, 0, 0], [0, 0, 0]],  # Step 1
+    ]).astype(np.float32)
+
     midi = convert(hov)
+    drum_track: mido.MidiTrack = midi.tracks[1]
+    use_absolute_time(drum_track)
 
     # Note: PrettyMIDI uses note_on events with velocity 0 as note_off, which is fine by the MIDI standard
-    drum_track: mido.MidiTrack = midi.tracks[1]
-    assert mido.Message("note_on", channel=9, note=35, velocity=127, time=0) in drum_track
+    assert mido.Message("note_on", channel=9, note=35, velocity=64, time=0) in drum_track
     assert mido.Message("note_on", channel=9, note=35, velocity=0, time=TICKS_PER_STEP) in drum_track
-    assert mido.MetaMessage("end_of_track", time=1) in drum_track
+    assert mido.MetaMessage("end_of_track", time=TICKS_PER_STEP + 1) in drum_track
+
+
+def test_hits_and_velocities():
+    hov = np.array([
+        [[1, 0, 127 / 127], [0, 0, 0], [0, 0, 0]],  # Kick
+        [[0, 0, 0], [0, 0, 0], [1, 0, 64 / 127]],  # Hi-Hat
+        [[1, 0, 127 / 127], [1, 0, 100 / 127], [0, 0, 0]],  # Kick + Snare
+        [[0, 0, 0], [0, 0, 0], [1, 0, 1 / 127]],  # Hi-Hat
+    ]).astype(np.float32)
+
+    midi = convert(hov)
+    drum_track: mido.MidiTrack = midi.tracks[1]
+    use_absolute_time(drum_track)
+
+    # Check if all note ons exist with the correct pitch and velocity
+    assert mido.Message("note_on", channel=9, note=35, velocity=127, time=0 * TICKS_PER_STEP) in drum_track
+    assert mido.Message("note_on", channel=9, note=42, velocity=64, time=1 * TICKS_PER_STEP) in drum_track
+    assert mido.Message("note_on", channel=9, note=35, velocity=127, time=2 * TICKS_PER_STEP) in drum_track
+    assert mido.Message("note_on", channel=9, note=37, velocity=100, time=2 * TICKS_PER_STEP) in drum_track
+    assert mido.Message("note_on", channel=9, note=42, velocity=1, time=3 * TICKS_PER_STEP) in drum_track
+
+
+def test_offsets():
+    hov = np.array([
+        [[1, 0, 1], [0, 0, 0], [0, 0, 0]],
+        [[1, 0.25, 1], [0, 0, 0], [0, 0, 0]],
+        [[1, 0.49, 1], [0, 0, 0], [0, 0, 0]],
+        [[1, -0.49, 1], [0, 0, 0], [0, 0, 0]],
+        [[1, -0.25, 1], [0, 0, 0], [0, 0, 0]],
+    ]).astype(np.float32)
+
+    midi = convert(hov)
+    drum_track: mido.MidiTrack = midi.tracks[1]
+    use_absolute_time(drum_track)
+
+    # Check if all the offsets get applied correctly
+    assert mido.Message("note_on", channel=9, note=35, velocity=127, time=round(0 * TICKS_PER_STEP)) in drum_track
+    assert mido.Message("note_on", channel=9, note=35, velocity=127, time=round(1.25 * TICKS_PER_STEP)) in drum_track
+    assert mido.Message("note_on", channel=9, note=35, velocity=127, time=round(2.49 * TICKS_PER_STEP), skip_checks=True) in drum_track
+    assert mido.Message("note_on", channel=9, note=35, velocity=127, time=round(2.51 * TICKS_PER_STEP), skip_checks=True) in drum_track
+    assert mido.Message("note_on", channel=9, note=35, velocity=127, time=round(3.75 * TICKS_PER_STEP), skip_checks=True) in drum_track
