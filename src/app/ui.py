a5257377b028e2543693871df4db71baa02419d1
Amon Benson <amonkbenson@gmail.com>
2026-02-05 18:14:19 +0100

    added checkpoint selection dialog

--- src/app/ui.py
+++ src/app/ui.py
@@ -1,7 +1,7 @@
 import asyncio
 from pathlib import Path
 from threading import Lock
-from typing import Callable
+from typing import Callable, Optional
 
 import flet as ft
 import flet.canvas as cv
@@ -51,7 +51,6 @@ class LabeledSlider(ft.Row):
 class SequenceCanvas(cv.Canvas):
     def __init__(
         self,
-        predictor: Predictor,
         predictor_lock: Lock,
         get_offset_bias: Callable[[], float],
         get_offset_gain: Callable[[], float],
@@ -61,13 +60,16 @@ class SequenceCanvas(cv.Canvas):
     ):
         super().__init__(height=height, expand=True, on_resize=self._handle_resize)
 
-        self._predictor = predictor
+        self._predictor: Optional[Predictor] = None
         self._predictor_lock = predictor_lock
         self._get_offset_bias = get_offset_bias
         self._get_offset_gain = get_offset_gain
         self._get_velocity_bias = get_velocity_bias
         self._get_velocity_gain = get_velocity_gain
         self._size = (1, 1)
+
+    def set_predictor(self, predictor: Predictor):
+        self._predictor = predictor
 
     def _handle_resize(self, e: cv.CanvasResizeEvent):
         self._size = (int(e.width), int(e.height))
@@ -139,13 +141,14 @@ class SequenceCanvas(cv.Canvas):
         w, h = self._size
         background = cv.Rect(0, 0, w, h, paint=ft.Paint(color=ft.Colors.SURFACE_CONTAINER))
 
+        if self._predictor is None:
+            self.shapes = [background]
+            self.update()
+            return
+
         with self._predictor_lock:
             sequence = self._predictor.get_context_sequence().numpy(force=True)
             position = self._predictor.get_position()
-
-        # window_size = 32
-        # display_end = (position // window_size + 1) * window_size
-        # sequence = full_sequence[:display_end]
 
         sequence_shapes = self._build_sequence_shapes(sequence, position, w, h)
 
@@ -166,21 +169,15 @@ def main(page: ft.Page):
     category_reverse_lookup = DrumCategory.generate_reverse_lookup(CONFIG.model.drums.categories)
 
     engine = MidiEngine()
-    predictor = Predictor(
-        PredictorConfig(
-            checkpoint=Path(".data/checkpoints/cp_20260201051208.pt"),
-            model=CONFIG.model,
-        )
-    )
+    predictor: Optional[Predictor] = None
     predictor_lock = Lock()
 
-    offset_bias_slider = LabeledSlider(value=0.05, min=-1.0, max=1.0, label="Offset Bias")
+    offset_bias_slider = LabeledSlider(value=0.1, min=-1.0, max=1.0, label="Offset Bias")
     offset_gain_slider = LabeledSlider(value=1.0, min=0.1, max=10.0, label="Offset Gain")
     velocity_bias_slider = LabeledSlider(value=0.3, min=-1.0, max=1.0, label="Velocity Bias")
     velocity_gain_slider = LabeledSlider(value=0.75, min=0.1, max=2.0, label="Velocity Gain")
 
     sequence_canvas = SequenceCanvas(
-        predictor=predictor,
         predictor_lock=predictor_lock,
         get_offset_bias=lambda: offset_bias_slider.current_value,
         get_offset_gain=lambda: offset_gain_slider.current_value,
@@ -190,7 +187,39 @@ def main(page: ft.Page):
 
     transport_display = TransportDisplay()
 
+    async def on_checkpoint_button_click(_):
+        nonlocal predictor
+        files = await ft.FilePicker().pick_files(
+            allowed_extensions=["pt"],
+            dialog_title="Select Checkpoint",
+            initial_directory=Path(CONFIG.train.checkpoint_dir).absolute().as_posix(),
+        )
+        if not files or not files[0].path:
+            return
+
+        checkpoint_path = Path(files[0].path)
+        with predictor_lock:
+            predictor = Predictor(PredictorConfig(checkpoint=checkpoint_path, model=CONFIG.model))
+            sequence_canvas.set_predictor(predictor)
+
+        checkpoint_button.content = checkpoint_path.name
+
+        page.update()
+
+    checkpoint_button = ft.ElevatedButton(
+        "Load Checkpoint",
+        icon=ft.Icons.FOLDER_OPEN,
+        style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=4)),
+        width=250,
+        height=46,
+        expand=False,
+        on_click=on_checkpoint_button_click,
+    )
+
     def handle_step(input_velocities: np.ndarray, step_position: int) -> tuple[np.ndarray, np.ndarray]:
+        if predictor is None:
+            return np.zeros(128, dtype=np.float32), np.zeros(128, dtype=np.uint8)
+
         num_instruments = CONFIG.model.drums.num_instruments
 
         triggered_categories = category_lookup[input_velocities > 0]
@@ -258,8 +287,8 @@ def main(page: ft.Page):
         engine.stop()
 
     page.title = "Drum Dequantization"
-    page.window.width = 700
-    page.window.height = 500
+    page.window.width = 600
+    page.window.height = 400
     page.window.always_on_top = True
     page.on_close = on_close
     page.theme = ft.Theme(color_scheme=ft.ColorScheme(primary=ft.Colors.AMBER))
@@ -268,7 +297,7 @@ def main(page: ft.Page):
     page.add(
         ft.Column(
             [
-                ft.Row([input_dropdown, output_dropdown]),
+                ft.Row([checkpoint_button, input_dropdown, output_dropdown]),
                 transport_display,
                 offset_bias_slider,
                 offset_gain_slider,
